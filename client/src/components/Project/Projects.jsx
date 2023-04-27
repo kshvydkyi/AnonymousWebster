25fc21c375b6b095f5c2b7405513eea309f1ff15
Kostyantyn Shvydkyi <72373801+kshvydkyi@users.noreply.github.com>
2023-04-27 17:50:36 +0300

    not finished create project page

--- client/src/components/Project/Projects.jsx
+++ client/src/components/Project/Projects.jsx
@@ -1,55 +1,89 @@
 import { FC, ReactElement } from 'react';
-import { Container, ElementsContainer, TextBlock, PaperComponent } from './ProjectsStyle';
-import { List, Card,CardActionArea, CardMedia, CardContent, Typography} from '@mui/material';
-
+import { Container, ElementsContainer, TextBlock, PaperComponent, ProjectCard, DateChip } from './ProjectsStyle';
+import { List, Card, CardActionArea, CardMedia, CardContent, Typography, Chip, Grid } from '@mui/material';
+import testPic from '../../assets/test/image 2.png'
+import newProjectIcon from '../../assets/ProjectPage/newProgectIcon.png'
 function ShowProjects() {
     const projects = [
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
-        { title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 1, title: "test nfvkdslvnmdskl nfkdslnfkdslfndsklfnkdslfn dsml fsdlnfdsklfndskls", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 2, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 3, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 4, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 5, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 6, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 7, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 9, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 10, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 11, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 12, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 13, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
+        { id: 14, title: "test", description: "description test", json_file: "test_file.json", date_upd: "2023-04-27T10:01:32+0000" },
 
 
     ]
 
 
-
+    //fdsfdsfdfdsfds
     return (
         <Container>
             <TextBlock>
                 Recent projects
             </TextBlock>
             <ElementsContainer>
-                <PaperComponent>
-                    <List>
+                <PaperComponent className='project-paper'>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        spacing={2}>
+
+
+                        <Grid item xs={12} sm={8} md={6} lg={4} xl={2}>
+                            <ProjectCard >
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        height="182"
+                                        image={newProjectIcon}
+                                        alt="projectPic"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                        New project
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="white" >
+
+                                        </Typography>
+
+                                    </CardContent>
+                                </CardActionArea>
+                            </ProjectCard>
+                        </Grid>
                         {projects ? projects.map((item) => {
                             return (
-                                <Card sx={{ maxWidth: 345 }}>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            component="img"
-                                            height="140"
-                                            image="/static/images/cards/contemplative-reptile.jpg"
-                                            alt="green iguana"
-                                        />
-                                        <CardContent>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                {item.title}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                                species, ranging across all continents except Antarctica
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                </Card>
+                                <Grid item xs={12} sm={8} md={6} lg={4} xl={2}>
+                                    <ProjectCard >
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="150"
+                                                image={testPic}
+                                                alt="projectPic"
+                                            />
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    {item.title.length < 13 ? item.title : `${item.title.slice(0, 13)}...`}
+                                                </Typography>
+                                                <DateChip label={item.date_upd} variant="outlined" />
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </ProjectCard>
+                                </Grid>
                             )
                         }) : <></>}
-                    </List>
+
+                    </Grid>
                 </PaperComponent>
             </ElementsContainer>
         </Container>
--- client/src/components/Project/ProjectsStyle.jsx
+++ client/src/components/Project/ProjectsStyle.jsx
@@ -1,5 +1,5 @@
-import {styled} from '@mui/system';
-import {Button, Paper} from '@mui/material';
+import { styled } from '@mui/system';
+import { Button, Card, Chip, Paper } from '@mui/material';
 
 const Container = styled('div')({
     paddingLeft: '7%',
@@ -12,17 +12,42 @@ const Container = styled('div')({
 const TextBlock = styled('div')({
     borderLeft: '6px solid white',
     color: 'white',
-    fontSize: '40px',
+    fontSize: '30px',
     paddingLeft: '1.5%'
 });
 
 const ElementsContainer = styled('div')({
     paddingTop: '2%',
+    paddingBottom: '2%',
     color: 'white'
 });
 
-const PaperComponent = styled(Paper)({
-    maxHeight: 550, 
-    overflow: 'auto'
+const ProjectCard = styled(Card)({
+    maxWidth: "340px",
+    backgroundColor: "#101010",
+    color: "white",
 })
-export {Container, ElementsContainer, TextBlock, PaperComponent}
+const PaperComponent = styled(Paper)({
+    maxHeight: 550,
+    overflow: 'auto',
+    padding: "2%",
+    backgroundColor: "inherit",
+    "&::-webkit-scrollbar": {
+        width: 5
+    },
+    "&::-webkit-scrollbar-track": {
+        backgroundColor: "#222222"
+    },
+    "&::-webkit-scrollbar-thumb": {
+        backgroundColor: "#850000",
+        borderRadius: 10
+    }
+})
+
+const DateChip = styled(Chip)({
+    color: "white",
+    border: "none",
+    padding: 0,
+    margin: 0
+})
+export { Container, ElementsContainer, TextBlock, PaperComponent, ProjectCard, DateChip }
